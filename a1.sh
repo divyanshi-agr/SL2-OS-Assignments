8e42a614e6136699503129160339dd765c880cf8
Divyanshi Agrawal <divsagr3@gmail.com>
2020-08-16 00:34:15 +0530

    completed

--- a1.sh
+++ a1.sh
@@ -1,6 +1,157 @@
 #!/bin/sh
-
 filename=0
+
+#Creating a database
+create_file () {
+    echo "Enter name of file to be created: "
+    read filename
+    len=${#filename}
+
+    if [ $len -gt 0 ]
+    then  
+        if [ -f $filename ]
+        then echo "File already exists!\n"
+        else
+            touch $filename
+            echo "SR.\t\tDEPT\t\tHOD\t\tTOTAL STUDENTS" >> $filename
+            echo "File successfully created!\n" 
+        fi
+    else 
+        echo "File name can't be empty!\n"
+    fi
+}
+
+#Inserting records in the database
+insert_file () {
+    echo "Enter no. of records you want to insert:"
+    read recs
+    
+    while [ $recs -gt 0 ]
+    do
+
+        while (true)
+        do
+            echo "Enter record no.:"
+            read srno
+            len_srno=${#srno}
+            if [ $len_srno -gt 0 ]
+            then break 1
+            else   
+                echo "You need to enter record no.!"
+            fi
+        done
+
+        while (true)
+        do
+            echo "Enter department name:"
+            read dept
+            len_dept=${#dept}
+            if [ $len_dept -gt 0 ]
+            then break 1
+            else   
+                echo "You need to enter dept name!"
+            fi
+        done
+
+        while (true)
+        do
+            echo "Enter HOD of this department:"
+            read hod
+            len_hod=${#hod}
+            if [ $len_hod -gt 0 ]
+            then break 1
+            else   
+                echo "You need to enter HOD's name!"
+            fi
+        done
+
+        while (true)
+        do
+            echo "Enter no. of students in this dept:"
+            read stud
+            len_stud=${#stud}
+            if [ $len_stud -gt 0 ]
+            then break 1
+            else   
+                echo "You need to enter total students!"
+            fi
+        done
+
+        echo "$srno\t\t$dept\t\t$hod\t\t$stud" >> $filename 
+        recs=$((recs-1))      
+    done
+}
+
+#Displaying the database
+display_file () {
+    if [ -f $filename ]
+    then 
+        echo "The database is: \n"
+        cat $filename 
+    else
+        echo "Database doesn't exist!!\n"
+    fi 
+}
+
+ #Searching for a record in database
+ search_file () {
+    read -p "Enter the name to be searched:" search_name
+    lol="$(grep -i "$search_name" $filename)"
+    if [ "$lol" ]
+    then
+        echo "Record found!"
+        echo "$lol"
+    else echo "Record not found!"
+    fi  
+ }
+
+ #Deleteing a record in database
+ delete_file () {
+    read -p "Enter department to be deleted:" delete_dept
+    sed -i "/$delete_dept/d" $filename
+    echo "\nRecord deleted successfully!"
+ }
+
+ #Modifying a record in database
+ modify_file () {
+    echo "Choose which attribute you want to update:"
+    echo "1.Department name"
+    echo "2.HOD name"
+    echo "3.Total students"
+    read choice
+
+    if [ $choice -le 0 || $choice -gt 4 ]
+    then echo "Please enter valid option!"
+    else
+        case $choice in
+        1 ) read -p "Enter department name of record you wanna update : " dept_name
+            read -p "Enter new department name : " newdept
+            sed -i "s/$dept_name/$newdept/g" $filename
+            echo "Record updated successfully!"
+
+        ;;
+
+        2 ) read -p "Enter HOD name of record you wanna update : " hod_name
+            read -p "Enter new HOD name : " newhod
+            sed -i "s/$hod_name/$newhod/g" $filename
+            echo "Record updated successfully!"
+
+        ;;
+
+        3 ) read -p "Enter total students of record you wanna update : " tot_studs
+            read -p "Enter new total students : " newstuds
+            sed -i "s/$tot_studs/$newstuds/g" $filename
+            echo "Record updated successfully!"
+
+        ;;
+
+        * ) echo "Wrong option !!" ;;
+
+        esac
+
+    fi
+ }
+
 while (true)
 do
     #Printing Menu
@@ -21,153 +172,20 @@ do
 
     case $option in
 
-        #Creating a database
-        1 )   echo "Enter name of file to be created: "
-              read filename
-              len=${#filename}
-
-              if [ $len -gt 0 ]
-              then  
-                    if [ -f $filename ]
-                    then echo "File already exists!\n"
-                    else
-                        touch $filename
-                        echo "SR.\t\tDEPT\t\tHOD\t\tTOTAL STUDENTS" >> $filename
-                        echo "File successfully created!\n" 
-                    fi
-               else 
-                    echo "File name can't be empty!\n"
-              fi
-        ;;
-
-        #Inserting records in the database
-        2 )   echo "Enter no. of records you want to insert:"
-              read recs
-              
-              while [ $recs -gt 0 ]
-              do
-
-                    while (true)
-                    do
-                        echo "Enter record no.:"
-                        read srno
-                        len_srno=${#srno}
-                        if [ $len_srno -gt 0 ]
-                        then break 1
-                        else   
-                            echo "You need to enter record no.!"
-                        fi
-                    done
-
-                    while (true)
-                    do
-                        echo "Enter department name:"
-                        read dept
-                        len_dept=${#dept}
-                        if [ $len_dept -gt 0 ]
-                        then break 1
-                        else   
-                            echo "You need to enter dept name!"
-                        fi
-                    done
-
-                    while (true)
-                    do
-                        echo "Enter HOD of this department:"
-                        read hod
-                        len_hod=${#hod}
-                        if [ $len_hod -gt 0 ]
-                        then break 1
-                        else   
-                            echo "You need to enter HOD's name!"
-                        fi
-                    done
-
-                    while (true)
-                    do
-                        echo "Enter no. of students in this dept:"
-                        read stud
-                        len_stud=${#stud}
-                        if [ $len_stud -gt 0 ]
-                        then break 1
-                        else   
-                            echo "You need to enter total students!"
-                        fi
-                    done
-
-                    echo "$srno\t\t$dept\t\t$hod\t\t$stud" >> $filename 
-                    recs=$((recs-1))      
-              done          
-        ;;
-
-        #Displaying the database
-        3 )   if [ -f $filename ]
-             then 
-                    echo "The database is: \n"
-                    cat $filename 
-             else
-                    echo "Database doesn't exist!!\n"
-             fi          
-        ;;
-
-        #Searching for a record in database
-        4 )   read -p "Enter the name to be searched:" search_name
-              lol="$(grep -i "$search_name" $filename)"
-              if [ "$lol" ]
-              then
-                 echo "Record found!"
-                 echo "$lol"
-              else echo "Record not found!"
-              fi       
-        ;;
-
-        #Deleteing a record in database
-        5 )   read -p "Enter department to be deleted:" delete_dept
-              sed -i "/$delete_dept/d" $filename
-              echo "\nRecord deleted successfully!"       
-        ;;
-
-        #Modifying a record in database
-        6 )   echo "Choose which attribute you want to update:"
-              echo "1.Department name"
-              echo "2.HOD name"
-              echo "3.Total students"
-              read choice
-
-              if [ $choice -le 0 || $choice -gt 4 ]
-              then echo "Please enter valid option!"
-              else
-                    case $choice in
-                    1 ) read -p "Enter department name of record you wanna update : " dept_name
-                        read -p "Enter new department name : " newdept
-                        sed -i "s/$dept_name/$newdept/g" $filename
-                        echo "Record updated successfully!"
-
-                    ;;
-
-                    2 ) read -p "Enter HOD name of record you wanna update : " hod_name
-                        read -p "Enter new HOD name : " newhod
-                        sed -i "s/$hod_name/$newhod/g" $filename
-                        echo "Record updated successfully!"
-
-                    ;;
-
-                    3 ) read -p "Enter total students of record you wanna update : " tot_studs
-                        read -p "Enter new total students : " newstuds
-                        sed -i "s/$tot_studs/$newstuds/g" $filename
-                        echo "Record updated successfully!"
-
-                    ;;
-
-                    esac
-
-              fi
-        ;;
+        1 )  create_file ;;
         
-        #Exitting the program
+        2 )  insert_file ;;
+        
+        3 )  display_file ;;
+       
+        4 )  search_file ;;
+        
+        5 )  delete_file ;;
+        
+        6 )  modify_file ;;     
+        
         7 )   exit 0 ;;
 
-        #Default option
         * )   echo "Wrong option!" ;;
 
     esac
@@ -360,8 +378,6 @@ done
 # 2.HOD name
 # 3.Total students
 # 1
-# ./a1.sh: 133: [: missing ]
-# ./a1.sh: 133: 1: not found
 # Enter department name of record you wanna update : IT 
 # Enter new department name : ENTC   
 # Record updated successfully!
@@ -383,8 +399,6 @@ done
 # 2.HOD name
 # 3.Total students
 # 2
-# ./a1.sh: 133: [: missing ]
-# ./a1.sh: 133: 2: not found
 # Enter HOD name of record you wanna update : anant
 # Enter new HOD name : roy
 # Record updated successfully!
@@ -406,8 +420,6 @@ done
 # 2.HOD name
 # 3.Total students
 # 3
-# ./a1.sh: 133: [: missing ]
-# ./a1.sh: 133: 3: not found
 # Enter total students of record you wanna update : 180
 # Enter new total students : 270
 # Record updated successfully!
