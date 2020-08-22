3b3821dc75707cf819f1a8f4440419666449162f
Divyanshi Agrawal <divsagr3@gmail.com>
2020-08-22 20:13:25 +0530

    awk

---
+++ a2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    printf("I am in a2.c\n");
+    printf("PID of a2.c is: %d\n", getpid());
+    printf("PID of parent is: %d\n", getppid());
+
+    
+    // fork();
+
+    // printf("PID of newc is: %d\n", getpid());
+    // printf("PID of parent of new is: %d\n", getppid());
+
+    return 0;
+}
---
+++ fork1.c
@@ -0,0 +1,39 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    pid_t pid;
+    char *msg;
+    int n;
+
+    printf("Fork program starting...\n");
+    pid = fork();
+
+    switch (pid)
+    {
+    case -1:
+        perror("Fork failed! Process not created.");
+        exit(1);
+
+    case 0:
+        msg = "This is the child";
+        n = 5;
+        break;
+
+    default:
+        msg = "This is the parent";
+        n = 3;
+        break;
+    }
+
+    for (; n > 0; n--)
+    {
+        puts(msg);
+        sleep(1);
+    }
+
+    exit(0);
+}
